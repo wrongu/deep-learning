33dfed416a2764b279bef90b90ac7dc5e13d7b72
wrongu <rlange@ur.rochester.edu>
2015-08-14 14:06:12 -0400

    Maxout can handle reset(stdv) and limit kernel norms

--- Maxout.lua
+++ Maxout.lua
@@ -58,3 +58,25 @@ function Maxout:__init(nInputPlane, nOutputPlane, nMaxOver, kWc, kHc, dWc, dHc, 
 	self:add(max_pooling_layer)
 	self:add(reshape4d_to_3d)
 end
+
+function Maxout:updateParameters(learningRate)
+	parent.updateParameters(self, learningRate)
+	if self.kernelNormLimit then
+		local convLayer = self:get(1)
+		-- note that SpatialConvolution has a viewWeights() method that views as nOut x (nIn*kW*kH),
+		-- but we want to view as (nOut*nIn) x (kW*kH) to isolate each kernel as a row
+		local view = convLayer.weight:view(convLayer.weight:size(1)*convLayer.weight:size(2), -1)
+		-- ensure the L2-norm of each kernel does not exceed kernelNormLimit
+		-- (modifying view modifies convLayer.weight as well)
+		view:renorm(view, 2, 1, self.kernelNormLimit) -- '2' is L2 norm, '1' is slices along the first dimension
+	end
+end
+
+function Maxout:limitKernelNorm(limit)
+	self.kernelNormLimit = limit
+	return self
+end
+
+function Maxout:reset(stdv)
+	self:get(1):reset(stdv)
+end
--- mnist_maxoutnet.lua
+++ mnist_maxoutnet.lua
@@ -14,11 +14,11 @@ require 'Maxout'
 
 net = nn.Sequential()
 -- 1x28x28 input image
-net:add(nn.Maxout(1, 48, 2, 8, 8, 1, 1, 0, 0, 4, 4, 2, 2))
+net:add(nn.Maxout(1, 48, 2, 8, 8, 1, 1, 0, 0, 4, 4, 2, 2):limitKernelNorm(0.9))
 -- 48x9x9
-net:add(nn.Maxout(48, 48, 2, 8, 8, 1, 1, 3, 3, 4, 4, 2, 2))
+net:add(nn.Maxout(48, 48, 2, 8, 8, 1, 1, 3, 3, 4, 4, 2, 2):limitKernelNorm(1.9365))
 -- 48x3x3
-net:add(nn.Maxout(48, 24, 4, 8, 8, 1, 1, 3, 3, 2, 2, 2, 2))
+net:add(nn.Maxout(48, 24, 4, 8, 8, 1, 1, 3, 3, 2, 2, 2, 2):limitKernelNorm(1.9365))
 -- 24x1x1
 net:add(nn.Reshape(24))
 -- 24x1
@@ -26,4 +26,15 @@ net:add(nn.Linear(24,10))
 -- 10x1 output classes (note: class 1 is 0, class 10 is 9.. careful off off by one)
 net:add(nn.LogSoftMax())
 
-return net:float()
+function net:reset(bound)
+	-- custom bounds on weight initialization
+	bound = (bound or 0.005) / math.sqrt(3)
+	net:get(1):reset(bound)
+	net:get(2):reset(bound)
+	net:get(3):reset(bound)
+	net:get(5):reset(bound)
+
+	return self
+end
+
+return net:float():reset()
